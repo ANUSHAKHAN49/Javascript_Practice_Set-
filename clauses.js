ef3417bdeaf783955ff6f0ebd9e9634dc839cffc
ANUSHAKHAN49 <anushakhan47231@gmail.com>
2025-01-19 21:56:10 +0500

    Create clauses.js

---
+++ clauses.js
@@ -0,0 +1,16 @@
+// ** Finally clauese ** //
+
+try{
+  let o = 0;
+  console.log(program)
+  console.log("this is a program")
+}
+catch(err) {
+   console.log("this is an error")
+}
+finally{
+  console.log("I am a developer")
+  // close the file 
+  // exit the loop
+  // Exit to the log file
+}
