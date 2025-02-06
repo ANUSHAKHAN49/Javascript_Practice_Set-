59aa5c9f209d5b284c9455dbfff4587927231497
ANUSHAKHAN49 <anushakhan47231@gmail.com>
2025-02-06 23:21:36 +0500

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,17 @@
+// Global scope variable 
+
+// var
+// Example
+var a = "anusha";
+var a = 25;
+// you can do this
+// Block scope Variable
+
+// let 
+// Example
+let x = "john";
+let x = 19
+// you can not do this
+
+
+
